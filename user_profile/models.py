ac043c76c7c9985a4e625a9568c94f2d8fa2f49d
inv <inv@MacBook-Pro-de-chema.local>
2019-03-22 13:34:10 +0100

    añadir userprofile en admin para mostrar campo.

--- user_profile/models.py
+++ user_profile/models.py
@@ -8,3 +8,6 @@ class UserProfile(models.Model):
     zip_code = models.PositiveIntegerField()
     gender = models.CharField(max_length=10, choices=(('M', 'Male'), ('F', 'Female')))
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
